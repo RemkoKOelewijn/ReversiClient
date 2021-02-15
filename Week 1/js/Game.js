4ca6e65110473cd354dbc4011cc5c1f6bcf8a917
Remko Koelewijn <s1143456@student.windesheim.nl>
2021-02-15 15:35:54 +0100

    Code na maken van opdrachten week 2 (15-2-2021)

--- Week 1/js/Game.js
+++ Week 1/js/Game.js
@@ -1,10 +1,8 @@
 const Game = (function(url){
-    //Configuratie en state waarden
     let configMap = {
         apiUrl: url
     }
 
-    // Private function init
     const privateInit = function(){
         console.log(configMap.apiUrl);
     }
@@ -31,17 +29,49 @@ const Game = (function(url){
     })() 
 
     Game.Data = (function(){
-        console.log('hallo, vanuit module Data')
-
-        let configMap = {}
-
-        privateInit = function(){
-            console.log("dit is de private functie")
+        dataInit = function(environment){
+            stateMap.environment = environment 
+            if(environment == 'development'){
+                return getMockData()
+            } else if(environment == 'production'){
+                return _get('api/Spel/Beurt')
+            } else {
+                throw new Error("Environment " + environment + " is niet geldig!")
+            }        
         }
 
+        const configMap = {
+            mock: [
+                {
+                    url: 'api/Spel/Beurt',
+                    data: 0
+                }
+                ]
+        }
+        
+        let stateMap = {environment : 'development'}
+
+        const getMockData = function(){
+            const mockData = configMap.mock;
+            
+            return new Promise((resolve, reject) => {
+                resolve(mockData);
+            });
+        }
+
+        const _get = function(url){
+                return $.get(url)
+            .then(r => {
+                return r
+            }).catch(e => {
+                console.log(e.message);
+            });
+        }
+        
         return {
-            init: privateInit
-        }      
+            init : dataInit,
+            get : _get
+        }
     })() 
 
     Game.Model = (function(){
@@ -49,12 +79,32 @@ const Game = (function(url){
 
         let configMap = {}
 
+        let stateMap;
+
         privateInit = function(){
+            _getCurrentGameState();
             console.log("dit is de private functie")
         }
 
+        const _getGameState = function(){
+            //aanvraag via Game.Data
+            //controle of ontvangen data valide is
+            let gameState = Game.Data.get("/api/Spel/Beurt/<token>")
+            if(gameState == 0 || gameState == 1 || gameState == 2){
+                return gameState
+            } else {
+                throw new Error("Game state is niet goed ontvangen!")
+            }
+        }
+
+        const _getCurrentGameState = function(){
+            console.log("oke")
+            stateMap = setInterval(_getGameState, 2000)
+        }
+
         return {
-            init: privateInit
+            init: privateInit,
+            getGameState : _getGameState
         }      
     })() 
 
